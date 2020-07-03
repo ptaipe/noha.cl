15bbe2fe809710a2c9a824a2952b2bdded7d1bc6
SSanchez <ssanchez@norte.digital>
2020-07-03 00:21:11 -0400

    Noha4

--- index.php
+++ index.php
@@ -4,22 +4,22 @@
 		<script src="https://apis.google.com/js/platform.js" async defer></script>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=no"> 
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=no">
 		<meta name="google-signin-client_id" content="554781700782-0e42vcdoof308bd61h8tnmioiblhsvko.apps.googleusercontent.com">
 
-		<title>n o h a</title>
-	
-		<link rel="shortcut icon" href="imagenes/logo.ico"> 
+		<title>NoHa</title>
+
+		<link rel="shortcut icon" href="imagenes/logo.ico">
 		<link rel="stylesheet" type="text/css" href="css/tags.css"/>
 		<link rel="stylesheet" type="text/css" href="css/popup.css"/>
 		<link rel="stylesheet" type="text/css" href="css/style.css"/>
 		<link rel="stylesheet" type="text/css" href="css/styleMobile.css"/>
-		<link rel="stylesheet" type="text/css" href="css/formRegisUser.css"/>	
+		<link rel="stylesheet" type="text/css" href="css/formRegisUser.css"/>
 		<link rel="stylesheet" type="text/css" href="css/formularios.css"/>
-		
+
 
 		<link rel="stylesheet" type="text/css" href="bootstrap-4.3.1/dist/css/bootstrap.min.css">
-		
+
 		<script src="js/validarRut.js?v=1"></script>
 		<script src="js/bodySlider.js?v=1"></script>
 		<script type="text/javascript">
@@ -28,15 +28,15 @@
 			  $(this).next('.form-control-file').addClass("selected").html(fileName);
 			})
 		</script>
-		
-		
+
+
 	</head>
-	<body background="imagenes/fondo6.jpg">	
+	<body background="imagenes/fondo6.jpg">
 		<div id="precarga">
 			<!-- precarga de backgrounds -->
-		</div> 	
+		</div>
 
-		<!-- Tramites -->		
+		<!-- Tramites -->
 		<div id="tramite" class="panel">
 			<div id="tablaMenu">
 				<table class="table table-sm table-borderless">
@@ -46,7 +46,7 @@
 								<a href="#revisiónTec" >
 									<img id="menu2" src="imagenes/menuRevison.jpg" >
 								</a>
-								<label id="popup1">revision TECNICA</label>								
+								<label id="popup1">revision TECNICA</label>
 							</div>
 						</td>
 						<td>
@@ -57,7 +57,7 @@
 								<label id="popup2">ENVIO ENCOMIENDAS</label>
 							</div>
 						</td>
-					</tr>								
+					</tr>
 					<tr>
 						<td>
 							<div id="conf4">
@@ -65,7 +65,7 @@
 									<img id="menu4" src="imagenes/compras.jpg">
 								</a>
 								<label id="popup3">compra canastas</label>
-							</div>	
+							</div>
 						</td>
 						<td>
 							<div id="conf1">
@@ -75,19 +75,19 @@
 								<label id="popup4">COMPRA REGALOS</label>
 							</div>
 						</td>
-					</tr>						
+					</tr>
 				</table>
 				<a href="#">
 					<img id="cerrarMenu" src="imagenes/cerrar.png">
-				</a>				
-			</div>			
-		</div>		
+				</a>
+			</div>
+		</div>
 		<!-- /Trámites -->
-		
+
 		<!-- Compras -->
 		<div id="tienda" class="panel">
 			<div class="content">
-				<h2>Compras</h2>				
+				<h2>Compras</h2>
 			</div>
 		</div>
 		<!-- /Compras -->
@@ -99,10 +99,10 @@
 				<div class="testbox">
 				  <center><titulo class="tituloForm">Registro</titulo></center>
 
-				  <form action="">				     
+				  <form action="">
 					  <hr>
-					  
-					  <input type="text" name="rut" id="txtRut" maxlength="8" placeholder="Rut" required/>					  
+
+					  <input type="text" name="rut" id="txtRut" maxlength="8" placeholder="Rut" required/>
 					  <input type="text" name="dv" id="txtDv" maxlength="1" placeholder="Dv" required/>
 
 					  <input type="text" name="nombre" id="txtNombre" placeholder="Nombre" required/>
@@ -113,7 +113,7 @@
 					  <hr>
 					   <center><a href="#" class="button">Guardar</a></center>
 				  </form>
-				</div>	
+				</div>
 			</div>
 		</div>
 		<!-- /Nuevo Socio -->
@@ -123,17 +123,17 @@
 			<div  class="content">
 				<h2>NoHa</h2>
 				<p>
-					En la actualidad las personas no disponen de tanto tiempo, ya que, están atados a un trabajo muy demandante o después del trabajo van a sus estudios superiores durante la tarde. Este problema de cotidianidad afecta la vida familiar de las personas y los tiempos de ocio que les gustaría tener. Además este problema de falta de tiempo genera que las personas soliciten permisos a sus trabajos para realizar estos trámites sencillos, generando un inconveniente para las mismas personas que las solicitan.  También hay que señalar que al realizar todas estas cotidianidades generan una mala experiencia o se suele vivir malos ratos, ya que la fila en las sucursales de pagos, supermercados, tiendas para comprar regalos, realizar la revisión técnica, llevar a lavar el auto o realizar trámites en el registro civil, son demasiados largas y lentas. 
-				</p>			
-			</div>	
-		</div>		
+					En la actualidad las personas no disponen de tanto tiempo, ya que, están atados a un trabajo muy demandante o después del trabajo van a sus estudios superiores durante la tarde. Este problema de cotidianidad afecta la vida familiar de las personas y los tiempos de ocio que les gustaría tener. Además este problema de falta de tiempo genera que las personas soliciten permisos a sus trabajos para realizar estos trámites sencillos, generando un inconveniente para las mismas personas que las solicitan.  También hay que señalar que al realizar todas estas cotidianidades generan una mala experiencia o se suele vivir malos ratos, ya que la fila en las sucursales de pagos, supermercados, tiendas para comprar regalos, realizar la revisión técnica, llevar a lavar el auto o realizar trámites en el registro civil, son demasiados largas y lentas.
+				</p>
+			</div>
+		</div>
 		<!-- /Nuestro Servicio -->
 
 		<!--Interior de Menu de tramites -->
 
 		<div id="revisiónTec" class="panel">
-			<div class="content">				
-				  <form action="">	
+			<div class="content">
+				  <form action="">
 				  	<table class="table table-borderless table-sm" id="tablePedidoRT">
 				  		<tr>
 				  			<td colspan="2">
@@ -146,13 +146,13 @@
 								  <div class="input-group-prepend">
 								    <span class="form-control form-control-sm">Rut</span>
 								  </div>
-									<input type="text" class="form-control form-control-sm" name="rutCli" id="" maxlength="8" placeholder="ejemplo: 12345678" required/>					  				
-								</div>									
+									<input type="text" class="form-control form-control-sm" name="rutCli" id="" maxlength="8" placeholder="ejemplo: 12345678" required/>
+								</div>
 				  			</td>
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="txtPatente" id="" maxlength="6" placeholder="Patente sin guión ej: ffgg52" required/>
 				  			</td>
-				  		</tr>				  		
+				  		</tr>
 				  		<tr>
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="nombreCli" id="" placeholder="Nombre" required/>
@@ -160,7 +160,7 @@
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="añoAuto" id="" maxlength="4" placeholder="Año de Vehículo" required/>
 				  			</td>
-				  		</tr>				  		
+				  		</tr>
 				  		<tr>
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="apellidoCli" id="" placeholder="Apellido" required/>
@@ -173,7 +173,7 @@
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="DirecciónCli" id="" placeholder="Dirección" required/>
 				  			</td>
-				  			<td>	  					
+				  			<td>
 								<div class="container">
 								    <label class="custom-file" id="customFile">
 								        <input type="file" class="custom-file-input" id="exampleInputFile" aria-describedby="fileHelp">
@@ -181,7 +181,7 @@
 								    </label>
 								</div>
 				  			</td>
-				  		</tr>				  		
+				  		</tr>
 				  		<tr>
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="telefonoCli" id="" placeholder="Telefóno" required/>
@@ -189,7 +189,7 @@
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="plantarevisión" id="" placeholder="Planta de revisión técnica" required/>
 				  			</td>
-				  		</tr>				  	
+				  		</tr>
 				  		<tr>
 				  			<td>
 				  				<input type="text" class="form-control form-control-sm" name="" id="" placeholder="Correo electrónico" required/>
@@ -207,26 +207,26 @@
 				  		</tr>
 				  	</table>
 				  </form>
-				
+
 				<a href="#tramite" id="btnvolver">
 					<img id="volverrt" src="imagenes/volver.png">
-				</a>							
-							
+				</a>
+
 				<a href="#" id="btnvolver">
 					<img id="cerrar" src="imagenes/cerrar.png">
 				</a>
 			</div>
 		</div>
 
-		<div id="encargosEnvios" class="panel">				
+		<div id="encargosEnvios" class="panel">
 			<div class="content">
-				<h2>Encargos o envios</h2>	
+				<h2>Encargos o envios</h2>
 				<div id="frmDelivery">
 					<table class="table table-borderless table-sm" id="tableEnvios1">
 				  		<tbody>
 					  		<tr>
-					  			<td><p id="subtitle">Remitente</p></td>					  			
-					  		</tr>				  			
+					  			<td><p id="subtitle">Remitente</p></td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<div class="input-group">
@@ -234,41 +234,41 @@
 									    <span class="form-control form-control-sm">Rut</span>
 									  </div>
 										<input type="text" class="form-control form-control-sm" name="rutCli" id="RtxtRutCli" maxlength="9" placeholder="ejemplo: 123456789 - sin guión" required="">
-									</div>									
-					  			</td>				  			
-					  		</tr>				  		
+									</div>
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="nombreCli" id="RtxtNombreCli" placeholder="Nombre" required="">
-					  			</td>				  			
-					  		</tr>				  		
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="apellidoCli" id="RtxtApellidoCli" placeholder="Apellido" required="">
-					  			</td>				  			
+					  			</td>
 					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="DirecciónCli" id="RtxtDireccCli" placeholder="Dirección" required="">
-					  			</td>				  			
-					  		</tr>				  		
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="telefonoCli" id="RtxtTelefonoCli" placeholder="Telefóno" required="">
-					  			</td>				  			
-					  		</tr>				  	
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="emailCli" id="" placeholder="Correo electrónico" required="">
-					  			</td>				  			
-					  		</tr>					  		
+					  			</td>
+					  		</tr>
 					  	</tbody>
 				  </table>
 				  <table class="table table-borderless table-sm" id="tableEnvios2">
 				  		<tbody>
 				  			<tr>
-					  			<td><p id="subtitle">Destinatario</p></td>					  			
-					  		</tr>				  			
+					  			<td><p id="subtitle">Destinatario</p></td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<div class="input-group">
@@ -276,41 +276,41 @@
 									    <span class="form-control form-control-sm">Rut</span>
 									  </div>
 										<input type="text" class="form-control form-control-sm" name="rutCli" id="RtxtRutCli" maxlength="9" placeholder="ejemplo: 123456789 - sin guión" required="">
-									</div>									
-					  			</td>				  			
-					  		</tr>				  		
+									</div>
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="nombreCli" id="RtxtNombreCli" placeholder="Nombre" required="">
-					  			</td>				  			
-					  		</tr>				  		
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="apellidoCli" id="RtxtApellidoCli" placeholder="Apellido" required="">
-					  			</td>				  			
+					  			</td>
 					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="DirecciónCli" id="RtxtDireccCli" placeholder="Dirección" required="">
-					  			</td>				  			
-					  		</tr>				  		
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="telefonoCli" id="RtxtTelefonoCli" placeholder="Telefóno" required="">
-					  			</td>				  			
-					  		</tr>				  	
+					  			</td>
+					  		</tr>
 					  		<tr>
 					  			<td>
 					  				<input type="text" class="form-control form-control-sm" name="emailCli" id="" placeholder="Correo electrónico" required="">
-					  			</td>				  			
-					  		</tr>					  						  		
+					  			</td>
+					  		</tr>
 					  	</tbody>
 				  </table>
 				  <table id="tableEnvios3">
 				  	<tr>
 				  		<td><label>¿Cuanto Pesa?</label></td>
 
-				  		<td><input type="text" class="form-control form-control-sm" id="" placeholder="Kilogramos"></td>		  		
+				  		<td><input type="text" class="form-control form-control-sm" id="" placeholder="Kilogramos"></td>
 
 				  		<td colspan="3">
 				  			<select class="form-control form-control-sm">
@@ -325,40 +325,40 @@
 				  	<tr>
 				  		<td >
 				  			<label>¿Cuanto Mide?</label>
-				  		</td>				  		
-				  		<td>				  			
+				  		</td>
+				  		<td>
 				  			<input type="text" class="form-control form-control-sm" id="" placeholder="Largo en cm.">
-				  		</td>				  		
-				  		<td>				  			
+				  		</td>
+				  		<td>
 				  			<input type="text" class="form-control form-control-sm" id="" placeholder="Ancho en cm.">
-				  		</td>				  		
-				  		<td>				  			
+				  		</td>
+				  		<td>
 				  			<input type="text" class="form-control form-control-sm" id="" placeholder="Profundidad en cm.">
-				  		</td>				  		
+				  		</td>
 				  	</tr>
 				  </table>
 				  <center>
 	  					<a href="#" id="guardarrt" class="btn btn-primary btn-sm active" role="button">Guardar</a>
 	  			  </center>
 				</div>
-			</div>		
+			</div>
 		</div>
 
-		<div id="canastas" class="panel">				
+		<div id="canastas" class="panel">
 			<div class="content">
-				<h2>stock canastas</h2>	
+				<h2>stock canastas</h2>
 			</div>
 
-					
-		</div>			
-		
+
+		</div>
+
 		<div id="comprasVarias" class="panel">
-			<div class="content">				
+			<div class="content">
 				<h2>Compras Regalos u otros</h2>
-				
+
 				<div class="testbox3">
 				  <form action="">
-				  	<table id="tabla3" align="center">				  		
+				  	<table id="tabla3" align="center">
 				  		<tr>
 				  			<td>
 				  				<input type="text" name="rutCli" id="RtxtRutCli" maxlength="8" placeholder="Rut" required/>
@@ -411,18 +411,18 @@
 				  </form>
 				</div>
 				<a href="#tramite"><img id="btnRegresar" src="imagenes/regresar.png"></a>
-			</div>		
+			</div>
 		</div>
-		
-			
-		
+
+
+
 		<!--Registro de clientes -->
 		<div id="formNewCliente" class="panel">
 			<div class="content">
 				<h2>Registro Nuevo Cliente</h2>
 				<div class="testbox2">
 				  <form action="">
-				  	<table id="tabla2" align="center">				  		
+				  	<table id="tabla2" align="center">
 				  		<tr>
 				  			<td>
 				  				<input type="text" name="rutCli" id="txtRutCli" maxlength="8" placeholder="Rut" required/>
@@ -459,12 +459,12 @@
 				<a href="#"><img id="btnRegresar" src="imagenes/regresar.png"></a>
 			</div>
 		</div>
-		<!--/Registro de clientes -->	
-		
+		<!--/Registro de clientes -->
+
 		<!-- menu de navegacion -->
 		<div id="header">
 			<h1>Nosotros lo hacemos por ti</h1>
-			<ul id="navigation">				
+			<ul id="navigation">
 				<li>
 					<a id="tram" href="#tramite">Servicios</a>
 					<img id="ico" src="imagenes/iconoTramites.png">
@@ -497,7 +497,7 @@
 						<td>+56 227340487</td>
 						<td>+569 71920899 - +569 87549928</td>
 						<td>contacto@noha.cl</td>
-					</tr>				
+					</tr>
 				</table>
 			</div>
 		</footer>
